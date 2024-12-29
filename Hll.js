8b58e53720dd9af8d3955547548b777483e82ce0
Karimov <121497820+AGK-1@users.noreply.github.com>
2024-12-29 04:30:59 +0400

    Update Hll.js

--- Hll.js
+++ Hll.js
@@ -2,7 +2,7 @@ console.log("Hello")
 foreach with arr[]
   ['hello',5,12,85,855,711, 52,0]
 def myFunc(int: year, str: Name)
-    this.name = "Salam"
+    this.name = "Doilingo"
 
 for i in a:
     if a[i] > b[i]:
