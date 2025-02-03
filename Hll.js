563d5ca31e886e5e7cc124625f67059bd37890c8
Karimov <121497820+AGK-1@users.noreply.github.com>
2025-02-03 16:10:59 +0400

    Update Hll.js

--- Hll.js
+++ Hll.js
@@ -1,6 +1,6 @@
 console.log("Hello")
 foreach with arr[]
-  ['hello',20,12,887,5575,712, 53,0,89955,346]
+  ['hello',20,12,887,5575,712, 53,0,89955,347]
 def myFunc(int: year, str: Name)
     this.name = "Doilingo"
 
